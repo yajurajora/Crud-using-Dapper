93b8ef6683502634673bf3981c85891f45885ea0
yajurajora <yaju8460@gmail.com>
2021-10-04 09:45:21 +0530

    first commit

---
+++ CrudInDapper/Abstraction/IDapper.cs
@@ -0,0 +1,27 @@
+﻿using CrudInDapper.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+//using System.Linq.Expressions;
+
+namespace CrudInDapper.Abstraction
+{
+    public interface IDapper
+    {
+
+        DbConnection GetDbConnecton();
+
+        Task<List<Student>> GetAllStudent();
+
+        Task<Student> InsertStudent(Student student);
+        Task<Student> UpdateStudent(Student student);
+        Task<Student> DeleteStudent(int StudentId);
+        Task<Student> GetStudentById(int StudentId);
+       // ICollection<Student> GetPk(int pk);
+
+
+    }
+}
---
+++ CrudInDapper/Controllers/StudentController.cs
@@ -0,0 +1,109 @@
+﻿using CrudInDapper.Abstraction;
+using CrudInDapper.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace CrudInDapper.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly IDapper _dapper;
+        public StudentController(IDapper dapper)
+        {
+            _dapper = dapper;
+        }
+        // GET: StudentController
+        public async Task<IActionResult> Index()
+        {                    
+            return View(await _dapper.GetAllStudent());
+        }
+        
+
+        // GET: StudentController/Details/5
+        public async Task<ActionResult<Student>> Details(int id)
+        {
+            var student = await _dapper.GetStudentById(id);
+            return View(student);
+        }
+
+        // GET: StudentController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult<Student>> Create(Student student)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    await _dapper.InsertStudent(student);
+                    return RedirectToAction("Index");
+                }
+
+                return View(student);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Edit/5
+        public async Task<ActionResult<Student>> Edit(int id)
+        {
+            var student = await _dapper.GetStudentById(id);
+            return View(student);
+        }
+
+        // POST: StudentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult<Student>> Edit(int id,Student student)
+        {
+            try
+            {
+                // TODO: Add update logic here                
+                await _dapper.UpdateStudent(student);
+                return RedirectToAction("Index");
+            }
+            catch(Exception e)
+            {
+                return View(e.Message);
+            }
+        }
+
+        // GET: StudentController/Delete/5
+        public async Task<ActionResult<Student>> Delete(int id)
+        {
+            var student = await _dapper.GetStudentById(id);
+            return View(student);
+        }
+
+        // POST: StudentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult<Student>>Delete(Student student,int id)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                await _dapper.DeleteStudent(id);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
